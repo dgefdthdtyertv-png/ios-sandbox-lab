52eb1f8a0fa24e237a698a790bc046ce36535874
dgefdthdtyertv-png <dgefdthdtyertv@icloud.com>
2026-09-11 14:17:22 +0800

    Update test.m

--- test.m
+++ test.m
@@ -1,94 +1,74 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
-#include <xpc/xpc.h>
-#include <sandbox.h>
+#import <xpc/xpc.h>
+#include <sys/stat.h>
 
-static UIWindow *floatWindow;
-static UILabel *logLabel;
+static UIWindow *floatWindow = nil;
+static UILabel *logLabel = nil;
 static BOOL expanded = NO;
-static NSMutableString *logs;
+static NSString *logPath = @"/var/mobile/Documents/xpc_log.txt";
 
 static void log(NSString *msg) {
+    NSString *line = [NSString stringWithFormat:@"%@ %@\n", [NSDate date], msg];
     NSLog(@"[xpc_test] %@", msg);
-    [logs appendFormat:@"%@\n", msg];
-    [logLabel setText:logs];
-    [logs writeToFile:@"/var/mobile/Documents/xpc_log.txt" atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    FILE *f = fopen([logPath UTF8String], "a");
+    if (f) { fputs([line UTF8String], f); fclose(f); }
+    if (logLabel) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            logLabel.text = [logLabel.text stringByAppendingString:line];
+        });
+    }
 }
 
 static void test_xpc() {
-    logs = [NSMutableString string];
     log(@"=== XPC Test Start ===");
+    xpc_connection_t conn = xpc_connection_create_mach_service("com.apple.containermanagerd", NULL, XPC_CONNECTION_MACH_SERVICE_PRIVILEGED);
+    if (!conn) { log(@"xpc connect null"); return; }
     
-    // 1. 尝试连接 containermanagerd
-    xpc_connection_t conn = xpc_connection_create_mach_service(
-        "com.apple.containermanagerd",
-        NULL,
-        XPC_CONNECTION_MACH_SERVICE_PRIVILEGED
-    );
-    
-    if (!conn) {
-        log(@"[-] Failed to create XPC connection");
-        return;
-    }
-    log(@"[+] XPC connection created");
-    
-    // 2. 设置事件处理器
     xpc_connection_set_event_handler(conn, ^(xpc_object_t event) {
-        if (xpc_get_type(event) == XPC_TYPE_ERROR) {
-            log([NSString stringWithFormat:@"[-] XPC error: %s", xpc_dictionary_get_string(event, XPC_ERROR_KEY_DESCRIPTION)]);
-        } else if (xpc_get_type(event) == XPC_TYPE_DICTIONARY) {
-            log(@"[+] Got XPC reply!");
-            // 打印所有 key
-            xpc_dictionary_apply(event, ^bool(const char *key, xpc_object_t value) {
-                log([NSString stringWithFormat:@"    key=%s type=%s", key, xpc_type_get_name(xpc_get_type(value))]);
-                if (xpc_get_type(value) == XPC_TYPE_STRING) {
-                    log([NSString stringWithFormat:@"    value=%s", xpc_string_get_string_ptr(value)]);
-                }
-                if (xpc_get_type(value) == XPC_TYPE_INT64) {
-                    log([NSString stringWithFormat:@"    value=%lld", xpc_int64_get_value(value)]);
-                }
-                return true;
-            });
-        }
+        log([NSString stringWithFormat:@"xpc event: %@", event]);
     });
-    
-    // 3. 发送查询消息（参考 bad_query 的查询方式）
-    xpc_object_t msg = xpc_dictionary_create(NULL, NULL, 0);
-    xpc_dictionary_set_string(msg, "operation", "get_container_extensions");
-    xpc_dictionary_set_string(msg, "bundle_id", "com.apple.filza");
-    xpc_dictionary_set_string(msg, "container_class", "appData");
-    xpc_dictionary_set_bool(msg, "include_sandbox_extensions", true);
-    
-    log(@"[+] Sending XPC query...");
-    xpc_connection_send_message(conn, msg);
-    xpc_release(msg);
     xpc_connection_resume(conn);
-    log(@"[+] XPC message sent, waiting for reply...");
     
-    // 4. 测试容器访问
-    NSArray *paths = @[
-        @"/var/mobile/Containers/Data/Application",
-        @"/var/mobile/Containers/Shared/AppGroup",
-        @"/var/containers/Data/System"
-    ];
+    xpc_object_t msg = xpc_dictionary_create(NULL, NULL, 0);
+    xpc_dictionary_set_string(msg, "command", "query");
+    xpc_dictionary_set_string(msg, "key", "sandbox_extensions");
+    
+    xpc_object_t reply = xpc_connection_send_message_with_reply_sync(conn, msg);
+    if (reply) {
+        log([NSString stringWithFormat:@"xpc reply: %@", reply]);
+        const char *token = xpc_dictionary_get_string(reply, "token");
+        if (token) log([NSString stringWithFormat:@"got token: %s", token]);
+    } else {
+        log(@"xpc no reply");
+    }
+    
+    // 路径探测
+    NSArray *paths = @[@"/var/mobile/Containers/Data/Application", @"/var/mobile/Containers/Shared/AppGroup"];
     for (NSString *p in paths) {
         BOOL exists = [[NSFileManager defaultManager] fileExistsAtPath:p];
         log([NSString stringWithFormat:@"path %@ exists=%d", p, exists]);
-        if (exists) {
-            NSError *err = nil;
-            NSArray *c = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:p error:&err];
-            if (err) {
-                log([NSString stringWithFormat:@"  list error: %@", err.localizedDescription]);
-            } else {
-                log([NSString stringWithFormat:@"  items: %lu", (unsigned long)c.count]);
-            }
-        }
     }
-    
     log(@"=== Test End ===");
 }
 
-// 悬浮窗
+@interface FloatBallView : UIView
+@end
+@implementation FloatBallView
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    expanded = !expanded;
+    logLabel.hidden = !expanded;
+    CGRect screen = [UIScreen mainScreen].bounds;
+    if (expanded) {
+        self.frame = CGRectMake(0, 0, 300, 400);
+        floatWindow.frame = CGRectMake(screen.size.width - 320, 50, 300, 400);
+    } else {
+        self.frame = CGRectMake(0, 0, 50, 50);
+        floatWindow.frame = CGRectMake(screen.size.width - 60, 100, 50, 50);
+    }
+}
+@end
+
 static void create_float_window() {
     dispatch_async(dispatch_get_main_queue(), ^{
         CGRect screen = [UIScreen mainScreen].bounds;
@@ -97,22 +77,18 @@ static void create_float_window() {
         floatWindow.backgroundColor = [UIColor clearColor];
         floatWindow.hidden = NO;
         
-        UIView *ball = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+        FloatBallView *ball = [[FloatBallView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
         ball.backgroundColor = [UIColor redColor];
         ball.layer.cornerRadius = 25;
         ball.layer.borderWidth = 2;
         ball.layer.borderColor = [UIColor whiteColor].CGColor;
-        ball.userInteractionEnabled = YES;
         
         UILabel *icon = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
-        icon.text = @"X";
-        icon.textColor = [UIColor whiteColor];
-        icon.font = [UIFont boldSystemFontOfSize:20];
-        icon.textAlignment = NSTextAlignmentCenter;
+        icon.text = @"X"; icon.textColor = [UIColor whiteColor];
+        icon.font = [UIFont boldSystemFontOfSize:20]; icon.textAlignment = NSTextAlignmentCenter;
         [ball addSubview:icon];
         
-        // 日志面板（默认隐藏）
-        logLabel = [[UILabel alloc] initWithFrame:CGRectMake(-200, 60, 250, 400)];
+        logLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 60, 300, 340)];
         logLabel.backgroundColor = [UIColor blackColor];
         logLabel.textColor = [UIColor greenColor];
         logLabel.font = [UIFont systemFontOfSize:10];
@@ -122,27 +98,7 @@ static void create_float_window() {
         logLabel.hidden = YES;
         [ball addSubview:logLabel];
         
-        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:nil action:nil];
-        [ball addGestureRecognizer:tap];
-        
-        // 用 block 处理点击
-        static dispatch_once_t once;
-        dispatch_once(&once, ^{
-            [[NSNotificationCenter defaultCenter] addObserverForName:@"tap" object:nil queue:nil usingBlock:^(NSNotification *n){
-                expanded = !expanded;
-                logLabel.hidden = !expanded;
-                ball.frame = expanded ? CGRectMake(-200, 0, 300, 500) : CGRectMake(0, 0, 50, 50);
-                floatWindow.frame = expanded ? CGRectMake(screen.size.width - 320, 50, 300, 500) : CGRectMake(screen.size.width - 60, 100, 50, 50);
-            }];
-        });
-        
-        tap.addTarget = ^(id target, SEL action){
-            [[NSNotificationCenter defaultCenter] postNotificationName:@"tap" object:nil];
-        };
-        
         [floatWindow addSubview:ball];
-        
-        // 延迟执行测试（等 UI 起来）
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
             test_xpc();
         });
